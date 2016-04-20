a22a39ce02221c161435c2c19d127b5745401352
Cagdas Korkut <c.korkut@dhbbank.com>
2016-04-20 13:56:01 +0300

    api project added but the previous functionality remains

---
+++ Authentication.API/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Authentication.API
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
