395c00459100aeb602c8f1bd1bc5348ab024404d
Cagdas Korkut <c.korkut@dhbbank.com>
2016-04-21 17:56:32 +0300

    started angular implementation. -checkpoint
    removed the aspx file as I've decided to keep the client side html-js only

---
+++ MArchiveAngularWeb/app/app.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('MovieApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    })
+        .when("/login", {
+            controller: "loginController",
+            templateUrl: "/app/views/login.html"
+        })
+        .when("/signup", {
+            controller: "signupController",
+            templateUrl: "/app/views/signup.html"
+        })
+        .when("/my-rated-movies", {
+            controller: "myRatedMoviesController",
+            templateUrl: "/app/views/myRatedMovies.html"
+        })
+        .otherwise({ redirectTo: "/home" });
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
