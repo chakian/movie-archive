4ee93eaccee47960c031a62458e5a2ba721767ce
Cagdas Korkut <c.korkut@dhbbank.com>
2016-04-22 13:51:32 +0300

    still doing minor changes

--- MArchiveAngularWeb/app/services/authInterceptorService.js
+++ MArchiveAngularWeb/app/services/authInterceptorService.js
@@ -18,6 +18,7 @@ app.factory('authInterceptorService', ['$q', '$location', 'localStorageService',
     var _responseError = function (rejection) {
         if (rejection.status === 401) {
             $location.path('/login');
+            localStorageService.remove ('authorizationData');
         }
         return $q.reject(rejection);
     }
